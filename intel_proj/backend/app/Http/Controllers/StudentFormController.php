fbab823cea10d7f2e8b689ffe6db716106048dd9
Thulasi <thulasipanndeshwara02@gmail.com>
2025-05-16 12:50:00 +0530

    litbackend

---
+++ intel_proj/backend/app/Http/Controllers/StudentFormController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentForm;
+use Illuminate\Http\Request;
+
+class StudentFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StudentForm  $studentForm
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StudentForm $studentForm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\StudentForm  $studentForm
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StudentForm $studentForm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\StudentForm  $studentForm
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StudentForm $studentForm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StudentForm  $studentForm
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StudentForm $studentForm)
+    {
+        //
+    }
+}
